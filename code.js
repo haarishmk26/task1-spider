ecb6b65475d4c6278232ee97070dcd2155fc99b6
haarishmk26 <44740893+haarishmk26@users.noreply.github.com>
2019-06-22 21:31:30 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
- var cvs = document.getElementById("canvas");
+   var cvs = document.getElementById("canvas");
     var ctx = cvs.getContext('2d');
     var bg = new Image();
     var speed = 1.5,i=1,e; 
@@ -8,6 +8,7 @@
     var game;
     var p=0;
     var keys;
+    var topspot,loser;
     document.addEventListener("keypress", movedown, false);
     
     function movedown(e) {
@@ -31,9 +32,18 @@
      }
 
     }
+    //let obstacle=[];
+    //obstacle[0]=new rectangle;
+    //let gamerno=[2];
+    //gamerno[0]= new obstacle;
+    //var obstacle=new array();
     let obstacle=[];
-    obstacle[0]=new rectangle;
-
+    for(o=0;0<2;o++)
+    {//obstacle[0]=new array();
+     obstacle[o]=[];}
+    obstacle[0][0]=new rectangle;
+    obstacle[1][0]=new rectangle; 
+    
     class circle{
       constructor(x,y,r)
       {
@@ -46,23 +56,36 @@
     ball[0]=new circle(400,400,15);//boy
     ball[1]=new circle(200,400,15);//girl
     //red ball is girl and blue is boy 0=boy 1=girl
-//class player{
-//constructor(myname,myscore)
-//{this.name=myname;
- // this.score=myscore;
-  //this.chance=mychance;
+class player{
+constructor(myname,myscore)
+{this.name=myname;
+  this.score=myscore;
+  this.chance=mychance;
+  
+
+}
+}
+//function displayLeaderboard() {
+  
+  //document.getElementById("scoreboard").innerHTML =<tr><td>gamer[topspot].name</td><td>gamer[topshpot].score</td></tr>;
+  //document.getElementById("scoreboard").innerHTML =<tr><td>gamer[loser].name</td><td>gamer[loser].score</td></tr>;
 
 //}
 
 
+let gamer=[2];
+gamer[0]=new player(Monkey,0,1);
+gamer[1]=new player(donkey,0,1);
 
-
-
-//}
-//let gamers=[2];
-//gamers[p]=new player(Monkey,0,1);
-//gamers[p]=new player(donkey,0,1);
     
+function bigscore()
+{if(gamer[0].score>gamer[1].score)
+{topspot=0;
+loser=1;}
+ else {topspot=1;loser=0;}
+}
+
+ 
    
     
     
@@ -84,10 +107,8 @@
      ctx.fill();
     }
     function reload()
-    { //if(!player2.chance)
+    { 
       location.reload();
-      //else
-      //clearRect
     }
     setInterval(decide,10);
     function decide()  //decide(e)
@@ -131,7 +152,7 @@
 function gamepause()
 { if (!gamePaused) 
    {
-    //game=clearTimeout(duetgame);
+    
     gamePaused = true;
    } 
  else if (gamePaused) 
@@ -147,31 +168,31 @@ function gamepause()
   
     
     function duetgame()
-    {  
-       if(gameover)
-       if(!gamePaused)
-      {{ ctx.drawImage(bg,0,0,600,600);
-        ctx.beginPath();
-        ctx.arc(300,400,100,0,(Math.PI*2),true);
-        ctx.strokeStyle="white";
-        ctx.stroke();
-        ctx.closePath();
-        circledraw(ball[0].x,ball[0].y,ball[0].r,"blue");
+    { 
+       if(gamer[1].chance)//if(gameover)
+       {if(!gamePaused)
+        { ctx.drawImage(bg,0,0,600,600);
+         ctx.beginPath();
+         ctx.arc(300,400,100,0,(Math.PI*2),true);
+         ctx.strokeStyle="white";
+         ctx.stroke();
+         ctx.closePath();
+         circledraw(ball[0].x,ball[0].y,ball[0].r,"blue");
         circledraw(ball[1].x,ball[1].y,ball[1].r,"red");
         ctx.beginPath();
         
         //RXis width  RYis height Xis xcoord of rectangle Ysimilarily
-        for(var j=0;j<obstacle.length;j++)
+        for(var j=0;j<obstacle[p].length;j++)
         {
-          ctx.fillRect(obstacle[j].X, obstacle[j].Y, obstacle[j].RX,obstacle[j].RY);
-          obstacle[j].Y+=speed;
+          ctx.fillRect(obstacle[p][j].X, obstacle[p][j].Y, obstacle[p][j].RX,obstacle[p][j].RY);
+          obstacle[p][j].Y+=speed;
           if(i==40)
           {speed+=0.2;
           i=1;}
-         if(obstacle[length].Y>200)
+         if(obstacle[p][length].Y>200)
          {length++;
 
-          obstacle.push({
+          obstacle[p].push({
            X:((Math.floor((Math.random() * cvs.width))/2)+150),
            Y:0,
           RX:Math.floor((Math.random()*80)+30),
@@ -181,40 +202,48 @@ function gamepause()
 
 
          } 
-         if(((ball[0].x>obstacle[j].X)&&(ball[0].x<obstacle[j].RX+obstacle[j].X))&&((ball[0].y>obstacle[j].Y)&&(ball[0].y<obstacle[j].RY+obstacle[j].Y)))         
-         {//location.reload();
-          //document.write("GAME OVER");
+         if(((ball[0].x>obstacle[p][j].X)&&(ball[0].x<obstacle[p][j].RX+obstacle[p][j].X))&&((ball[0].y>obstacle[p][j].Y)&&(ball[0].y<obstacle[p][j].RY+obstacle[p][j].Y)))         
+         {angle=0;
+          gamer[p].chance=0;
+          p++;
+          gamePaused = true;
           ctx.save();
-          gameover=0;
-          ctx.fillStyle="white";
+          //gameover=0;
+          if(!gamer[1].chance)
+          {ctx.fillStyle="white";
           ctx.font = "80px Arial";
           ctx.fillText("GAME OVER",50,300);
           ctx.restore();
-         
+         // displayLeaderboard();
+          }
+          //displayLeaderboard();} 
           break;
-          //document.location.reload();
-          //clearInterval(interval);
+      
          }
 
-         if(((ball[1].x>obstacle[j].X)&&(ball[1].x<obstacle[j].RX+obstacle[j].X))&&((ball[1].y>obstacle[j].Y)&&(ball[1].y<obstacle[j].RY+obstacle[j].Y)))
-         {
+         if(((ball[1].x>obstacle[p][j].X)&&(ball[1].x<obstacle[p][j].RX+obstacle[p][j].X))&&((ball[1].y>obstacle[p][j].Y)&&(ball[1].y<obstacle[p][j].RY+obstacle[p][j].Y)))
+         {angle=0; gamer[p].chance=0;p++;
+          gamePaused = true;
           ctx.save();
-          gameover=0;
-          ctx.fillStyle="white";
-          ctx.font = "80px Arial";
-          ctx.fillText("GAME OVER",50,300);
-          ctx.restore();
           
+          //gameover=0;
+          if(!gamer[1].chance)
+          {ctx.fillStyle="white";
+           ctx.font = "80px Arial";
+           ctx.fillText("GAME OVER",50,300);
+           ctx.restore();
+          //displayLeaderboard();
+         } //displayLeaderboard();
           break;
           
          }
 
         }
-        i++;score++;
+        i++;gamer[p].score++;
         
 
         ctx.fillStyle="white";
-        ctx.fillText("Score"+score,10,30);
+        ctx.fillText("Score"+gamer[p].score,10,30);
         ctx.font = "20px Arial";
         ctx.closePath();
         setTimeout(duetgame,10);
@@ -223,4 +252,4 @@ function gamepause()
       }
     }
      
-    }
+  }
